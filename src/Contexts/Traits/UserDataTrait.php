3ed6386381f0cecd637c9bceb8c1b819fdc8de3e
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-04-19 15:59:23 +0200

    ADD UserDataTrait for Ceres context classes - this trait contains user related information for my-account and checkout

---
+++ src/Contexts/Traits/UserDataTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ceres\Contexts\Traits;
+
+use IO\Services\CheckoutService;
+use IO\Services\ContactBankService;
+use IO\Services\CustomerService;
+
+trait UserDataTrait
+{
+    public $contact = null;
+
+    public $billingAddressList = [];
+    public $selectedBillingAddressId = null;
+
+    public $deliveryAddressList = [];
+    public $selectedDeliveryAddressId = null;
+
+    public $userBankData = [];
+
+    public $contactHasReturns = null;
+
+    protected function initUserData($params)
+    {
+        parent::init($params);
+
+        /** @var CustomerService $customerService */
+        $customerService = pluginApp( CustomerService::class );
+
+        /** @var CheckoutService $checkoutService*/
+        $checkoutService = pluginApp( CheckoutService::class );
+
+        /** @var ContactBankService $contactBankService */
+        $contactBankService = pluginApp( ContactBankService::class );
+
+        $this->contact = $customerService->getContact();
+
+        $this->billingAddressList = $customerService->getAddresses(1);
+        $this->selectedBillingAddressId = $checkoutService->getBillingAddressId();
+
+        $this->deliveryAddressList = $customerService->getAddresses(2);
+        $this->selectedDeliveryAddressId = $checkoutService->getDeliveryAddressId();
+
+        if(isset($this->contact))
+        {
+            $this->userBankData = $contactBankService->getBanksOfContact($this->contact->id);
+
+            $this->contactHasReturns = $customerService->hasReturns();
+        }
+    }
+}
